c78646f96a0a41661a01ebfea44cbdb29d2fd4d6
摸鱼喵 <33485524+Lavender-QAQ@users.noreply.github.com>
2022-05-13 19:29:18 +0800

    Complete correct dataflow (#15)

--- executer/argo/docker.go
+++ executer/argo/docker.go
@@ -35,8 +35,33 @@ func NewDockerNode(id string, image string, port int, target string, command []s
 		Port:    port,
 		Target:  target,
 		Command: command,
-		Args:    args,
 	}
+}
+
+func (node *DockerNode) GenerateTemplate() v1alpha1.Template {
+	template := v1alpha1.Template{
+		Name: node.GetId(),
+		Container: &v1.Container{
+			Image:   RequestImage,
+			Command: []string{"./agency"},
+		},
+	}
+
+	var args Args
+	if node.HaveInNode() && node.HaveOutNode() {
+		args = NewArgsWithInputAndOutput(node.GetInNode()[0], node.GetId(), node.Image, node.Port, node.Target)
+		template.Outputs.Artifacts = getTemplateArtifactsByOutcome(node.GetId())
+		template.Inputs.Artifacts = getTemplateArtifactsByIncome(node.GetInNode())
+	} else if node.HaveOutNode() {
+		template.Outputs.Artifacts = getTemplateArtifactsByOutcome(node.GetId())
+		args = NewArgsWithOutput(node.GetId(), node.Image, node.Port, node.Target)
+	} else if node.HaveInNode() {
+		template.Inputs.Artifacts = getTemplateArtifactsByIncome(node.GetInNode())
+		args = NewArgsWithInput(node.GetInNode()[0], node.Image, node.Port, node.Target)
+	}
+	template.Container.Args = args
+
+	return template
 }
 
 func (node *DockerNode) GenerateTemplate() v1alpha1.Template {
@@ -71,9 +96,12 @@ func buildDockerNode(e etree.Element, node_wg *sync.WaitGroup) {
 	id := e.SelectAttrValue("id", "none")
 	image := e.SelectAttrValue("image", "none")
 	portStr := e.SelectAttrValue("port", "none")
-	target := e.SelectAttrValue("target", "none")
+
+	target := e.SelectAttrValue("target", "/")
 	commandStr := e.SelectAttrValue("command", "none")
 	argsStr := e.SelectAttrValue("args", "none")
+
+	image = "httpserver"
 
 	port, err := strconv.Atoi(portStr)
 	if err != nil {
